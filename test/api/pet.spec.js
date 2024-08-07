848ff25d23220012f7ff57f3239944b136dce955
Vinicius38z <vinicius.meneses81@gmail.com>
2024-08-06 21:50:04 -0300

    Arquivos Iniciais

---
+++ test/api/pet.spec.js
@@ -0,0 +1,28 @@
+// Bibliotecas e Frameworks
+const supertest = require('supertest')
+const petid = 539781501
+
+
+// Em javascript, classe é opcional, mas pode agrupar em um describe
+    describe('API Petstore Swagger - Entidade Pet', () =>{
+    
+
+    // Atributos do grupo/describe
+   const resquest = supertest('https://petstore.swagger.io/v2') // Base URL
+    // Função ou metodos
+        it('Post Pet', () =>{
+
+
+        }) // Final do método POST
+
+    // Atributos, campos, caracteristicas e configurações
+
+
+
+    // Funções de apoio(opcional)
+
+
+    // Funções de Teste em si
+    })
+        // Final do Describe
+
